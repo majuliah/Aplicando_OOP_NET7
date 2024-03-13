bc93420627393b0ea325b3d30b8377257cb4ed98
Majulia <majulialcarvalho@gmail.com>
2024-03-12 22:14:03 -0300

    source generators

--- UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 
 namespace UtmBuilder.Core.ValueObjects.Exceptions;
 
-public class InvalidUrlException: Exception
+public partial class InvalidUrlException: Exception
 {
     private const string DefaultErrorMessage = "Invalid URL";
     private const string UrlRegexPattern = @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
@@ -16,10 +16,12 @@ public class InvalidUrlException: Exception
         if (string.IsNullOrEmpty(adress))
             throw new InvalidUrlException(message);
         
-        if (!Regex.IsMatch(adress, UrlRegexPattern))
+        if (!UrlRegex().IsMatch(adress))
         {
             throw new InvalidUrlException();
-            
         }
     }
+
+    [GeneratedRegex(UrlRegexPattern)]
+    private static partial Regex UrlRegex();
 }
