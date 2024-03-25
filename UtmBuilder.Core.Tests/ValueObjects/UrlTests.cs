3dae745475a0441f2c2788cead9e5694d0866c37
Majulia <majulialcarvalho@gmail.com>
2024-03-25 20:05:57 -0300

    expected exception

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -7,23 +7,16 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 public class UrlTests
 {
     [TestMethod("Deve retornar uma exceção quando URL for inválida")]
-    [TestCategory("Teste de URL")]
+    [ExpectedException(typeof(InvalidUrlException))]
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
-        try
-        {
-            var url = new Url("");
-            Assert.Fail();
-        }
-        catch (InvalidUrlException e)
-        {
-            Assert.IsTrue(true);
-        }
+        new Url("");
     }
     
     [TestMethod]
     public void ShouldNotReturnExceptionWhenUrlIsValid()
     {
-        Assert.Fail();
+        new Url("https://maju.io");
+        Assert.IsTrue(true);
     }
 }
